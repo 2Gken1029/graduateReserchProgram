83ab43b69f3c151d020141cb180bbbd7429dc854
2Gken1029 <ken1029crew@gmail.com>
2020-09-10 23:02:56 +0900

    雌株使うやつ

---
+++ mecabForGraduate.py
@@ -0,0 +1,30 @@
+#! python3
+# mecabForGraduate.py - mecabSampleCodeを改変
+
+# Mecabがどこにあるか調べるコマンド
+# $ echo `mecab-config --dicdir`"/mecab-ipadic-neologd"
+
+import MeCab, sys, os
+
+mecab = MeCab.Tagger('-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd') # -d以下はechoで調べたパス
+
+if len(sys.argv) < 2:
+    print("引数に文章ファイルが指定されていません.")
+    print("$ python3 mecabForGraduate.py [文章ファイル]")
+    sys.exit()
+
+string_file = open(sys.argv[1])
+string_list = string_file.readlines()
+string_file.close
+
+# リスト内文字列の"\n"を削除
+string_list = [s.replace('\n', '') for s in string_list]
+print(string_list)
+
+mecab.parse('')#文字列がGCされるのを防ぐ
+node = mecab.parseToNode(string_list)
+while node:
+    #単語を取得
+    word = node.surface
+    #次の単語に進める
+    node = node.next
